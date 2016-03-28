db023290ebd0deace87267eae5ccc2d248d82504
joshuagruenstein <joshuagruenstein@gmail.com>
2016-03-27 23:29:21 -0400

    forgot the top C

--- ThingTwo/Code/main.cpp
+++ ThingTwo/Code/main.cpp
@@ -6,12 +6,12 @@
 int main() {
     Hardware thingTwo;
 
-    int cMajor[] = {262,294,330,349,392,440,494};
+    int cMajor[] = {262,294,330,349,392,440,494,523};
 
-    for (int i=0; i<7; i++) {
+    for (int i=0; i<8; i++) {
         thingTwo.setBuzzer(cMajor[i]);
         usleep(500000);
-    } for (int i=5; i>=0; i--) {
+    } for (int i=6; i>=0; i--) {
         thingTwo.setBuzzer(cMajor[i]);
         usleep(500000);
     }
