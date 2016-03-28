de576729b8337b6d09712dd19a99762a11a6de29
Michael Truell <truell20@gmail.com>
2016-03-28 15:55:58 -0400

    Make line follow holonomic (no rotation)

--- ThingTwo/Code/Experiments/main.cpp
+++ ThingTwo/Code/Experiments/main.cpp
@@ -91,7 +91,7 @@ void goStraightKiwi() {
 }
 
 void lineFollow() {
-    double maxRotate = 100;
+    double maxDisplacementComponent = 100;
     double exploration = 0.2;
 
     Connection connection;
@@ -102,7 +102,7 @@ void lineFollow() {
 
     Hardware hardware;
 
-    rl::WireFitQLearn learner = rl::WireFitQLearn(1, 1, 1, 3, 4, {-1}, {1}, 11, new rl::LSInterpolator(), net::Backpropagation(0.01, 0.9, 0.1, 35000), 0.95, 0.4);
+    rl::WireFitQLearn learner = rl::WireFitQLearn(1, 2, 1, 6, 4, {-1, -1}, {1, 1}, 3, new rl::LSInterpolator(), net::Backpropagation(0.01, 0.9, 0.05, 5000), 1, 0.5);
     learner.reset();
     
     std::cout << "Done with initialization\n";
@@ -110,8 +110,8 @@ void lineFollow() {
         rl::Action action = learner.chooseBoltzmanAction({!isLeftOfLine(hardware) ? -1 : 1}, exploration);
         std::cout << "Action: " << action[0] << "\n";
 
-        hardware.goHolonomic(0, 50, action[0]*maxRotate);
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        hardware.goHolonomic(action[0]*maxDisplacementComponent, action[1]*maxDisplacementComponent, 0);
+        std::this_thread::sleep_for(std::chrono::milliseconds(400));
         hardware.goHolonomic(0, 0, 0);
         
         double reward = connection.getReward();
@@ -121,7 +121,7 @@ void lineFollow() {
 
     while(true) {
         rl::Action action = learner.chooseBoltzmanAction({!isLeftOfLine(hardware) ? -1 : 1}, 0.001);
-        hardware.goHolonomic(0, 50, action[0]*maxRotate);
+        hardware.goHolonomic(action[0]*maxDisplacementComponent, action[1]*maxDisplacementComponent, 0);
         std::this_thread::sleep_for(std::chrono::milliseconds(50));
         std::cout << "Action: " << action[0] << "\n";
     }
