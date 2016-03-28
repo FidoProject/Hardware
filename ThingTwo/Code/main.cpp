fce30bbdac7e61c901be87d73da364813fb80501
Josh Gruenstein <joshuagruenstein@gmail.com>
2016-03-28 16:15:49 +0000

    Fixes to the go straigt task

--- ThingTwo/Code/main.cpp
+++ ThingTwo/Code/main.cpp
@@ -4,7 +4,7 @@
 #include <thread>
 
 #include "Hardware.h"
-#include "Fido.h"
+#include "Fido/Fido.h"
 #include "../../Connection.h"
 
 void lSleep(double seconds) {
@@ -16,28 +16,38 @@ void goStraight() {
 	double exploration = 0.2;
 
 	Connection connection;
-	Hardware hardware;
+    int receiverNum;
+    do {
+        receiverNum = atoi(connection.getString().c_str());
+    } while(receiverNum != 5 && receiverNum != 6);
 
-	Simlink simulator;
+    Hardware hardware;
+
 	rl::WireFitQLearn learner = rl::WireFitQLearn(1, 1, 1, 4, 3, {-1}, {1}, 11, new rl::LSInterpolator(), net::Backpropagation(0.01, 0.9, 0.1, 35000), 0.95, 0.4);
 	learner.reset();
-
+    
+    std::cout << "Done with initialization\n";
 	while(true) {
 		rl::Action action = learner.chooseBoltzmanAction({1}, exploration);
 
 		hardware.goHolonomic(0, 50, action[0]*maxRotate);
 		std::this_thread::sleep_for(std::chrono::milliseconds(500));
 		hardware.goHolonomic(0, 0, 0);
-		
-		learner.applyReinforcementToLastAction(connection.getReward(), {1});
+	    
+        double reward = connection.getReward();
+        if(fabs(reward - (-2)) < 0.001) break;
+		learner.applyReinforcementToLastAction(reward, {1});
 	}
+
+    while(true) {
+        rl::Action action = learner.chooseBoltzmanAction({1}, exploration);
+        hardware.goHolonomic(0, 50, action[0]*maxRotate);
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    }
 }
 
 int main() {
-    Hardware thingTwo;
-
-    thingTwo.goHolonomic(0,100,20);
-    lSleep(3);
+    goStraight();
 
     return 0;
 }
