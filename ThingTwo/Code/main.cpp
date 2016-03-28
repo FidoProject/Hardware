d7de24d2ea4cf35a9f99e9a2cef653f35b4f9951
joshuagruenstein <joshuagruenstein@gmail.com>
2016-03-27 23:39:08 -0400

    fixed usleep to lSleep

--- ThingTwo/Code/main.cpp
+++ ThingTwo/Code/main.cpp
@@ -11,11 +11,11 @@ int main() {
     Hardware thingTwo;
 
     thingTwo.setMotors(100,0,0);
-    usleep(0.5);
+    lSleep(0.5);
     thingTwo.setMotors(0,100,0);
-    usleep(0.5);
+    lSleep(0.5);
     thingTwo.setMotors(0,0,100);
-    usleep(0.5);
+    lSleep(0.5);
 
     return 0;
 }
