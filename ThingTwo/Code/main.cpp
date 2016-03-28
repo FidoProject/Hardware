163eec4974a59c55d621431c6b6182a9e6c2b5db
Michael Truell <truell20@gmail.com>
2016-03-28 11:10:17 -0400

    go straight task

--- ThingTwo/Code/main.cpp
+++ ThingTwo/Code/main.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
 #include <unistd.h>
+#include <chrono>
+#include <thread>
 
 #include "Hardware.h"
+#include "Fido.h"
+#include "../../Connection.h"
 
 void lSleep(double seconds) {
     usleep(seconds*1000000.0);
+}
+
+void goStraight() {
+	double maxRotate = 100;
+	double exploration = 0.2;
+
+	Connection connection;
+	Hardware hardware;
+
+	Simlink simulator;
+	rl::WireFitQLearn learner = rl::WireFitQLearn(1, 1, 1, 4, 3, {-1}, {1}, 11, new rl::LSInterpolator(), net::Backpropagation(0.01, 0.9, 0.1, 35000), 0.95, 0.4);
+	learner.reset();
+
+	while(true) {
+		rl::Action action = learner.chooseBoltzmanAction({1}, exploration);
+
+		hardware.goHolonomic(0, 50, action[0]*maxRotate);
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		hardware.goHolonomic(0, 0, 0);
+		
+		learner.applyReinforcementToLastAction(connection.getReward(), {1});
+	}
 }
 
 int main() {
