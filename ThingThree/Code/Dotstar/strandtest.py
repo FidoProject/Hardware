058e66f6e01cbaae72a8501d86066032ebca3f10
Joshua Gruenstein <joshuagruenstein@gmail.com>
2016-04-24 21:55:48 +0000

    change bit shifting to green

--- ThingThree/Code/Dotstar/strandtest.py
+++ ThingThree/Code/Dotstar/strandtest.py
@@ -14,12 +14,12 @@ strip.setBrightness(255)
 
 for color in range(0,255):
 	for i in range(0,numpixels):
-		strip.setPixelColor(i, color << 8)
+		strip.setPixelColor(i, color << 16)
 	strip.show()
 	time.sleep(0.01)
 
 for color in range(255,0,-1):
 	for i in range(0,numpixels):
-		strip.setPixelColor(i, color << 8)
+		strip.setPixelColor(i, color << 16)
 	strip.show()
-	time.sleep(0.01)
+	time.sleep(0.001)
