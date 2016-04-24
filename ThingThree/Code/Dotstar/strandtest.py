ef6e3ef9c962b4e2647a3666cd7d148bcae77378
joshuagruenstein <joshuagruenstein@gmail.com>
2016-04-24 18:59:08 -0400

    generalize the fade

--- ThingThree/Code/Dotstar/strandtest.py
+++ ThingThree/Code/Dotstar/strandtest.py
@@ -3,23 +3,31 @@
 import time
 from dotstar import Adafruit_DotStar
 
-numpixels = 24
+numPixels = 24
 
-datapin   = 17
-clockpin  = 27
-strip     = Adafruit_DotStar(numpixels, datapin, clockpin)
+dataPin   = 17
+clockPin  = 27
+strip     = Adafruit_DotStar(numPixels, dataPin, clockPin)
 
 strip.begin()
 strip.setBrightness(255)
 
-for color in range(0,255):
-	for i in range(0,numpixels):
-		strip.setPixelColor(i, color << 16)
-	strip.show()
-	time.sleep(0.01)
+def scale(color, brightness):
+	str_hex = hex(color)[2:].zfill(6)
+	r,g,b = (int(str_hex[2*x:2*x+2],16)*(brightness/255.0) for x in xrange(3))
+	return (int(r) << 16) + (int(g) << 8) + int(b)
 
-for color in range(255,0,-1):
-	for i in range(0,numpixels):
-		strip.setPixelColor(i, color << 16)
-	strip.show()
-	time.sleep(0.001)
+def pulseFade(color):
+	for brightness in range(0,255):
+		for i in range(0,numPixels):
+			strip.setPixelColor(i, scale(color,brightness))
+		strip.show()
+		time.sleep(0.01)
+
+	for brightness in range(255,0,-1):
+		for i in range(0,numPixels):
+			strip.setPixelColor(i, scale(color,brightness))
+		strip.show()
+		time.sleep(0.001)
+
+pulseFade(0x00FF00)
